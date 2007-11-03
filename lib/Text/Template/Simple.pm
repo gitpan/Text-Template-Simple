91c77b93adc36d1d23d64dd49e070e1175c4e79f
Burak Gürsoy <burak@cpan.org>
2007-11-03 22:25:12 +0000

    Import of BURAK/Text-Template-Simple-0.49_01 from CPAN.
    gitpan-cpan-distribution: Text-Template-Simple
    gitpan-cpan-version:      0.49_01
    gitpan-cpan-path:         BURAK/Text-Template-Simple-0.49_01.tar.gz
    gitpan-cpan-author:       BURAK
    gitpan-cpan-maturity:     developer

--- lib/Text/Template/Simple.pm
+++ lib/Text/Template/Simple.pm
@@ -5,6 +5,14 @@ package Text::Template::Simple::Dummy;
 # them inside templates. It is also possible to declare
 # and share package variables under strict (safe mode can
 # have problems though). See the Pod for more info.
+use strict;
+
+sub stack { # just a wrapper
+   my $opt = shift || {};
+   die "Parameters to stack() must be a HASH" if ref($opt) ne 'HASH';
+   $opt->{frame} = 1;
+   Text::Template::Simple::Caller->stack( $opt );
+}
 
 package Text::Template::Simple::Compiler;
 # Compiling any code inside the template class is 
@@ -15,11 +23,13 @@ package Text::Template::Simple::Compiler;
 # all be accessible inside the template code...
 #
 # So, we explode the bomb in deep space instead ;)
+use strict;
 
 sub _compile { shift; return eval shift }
 
 package Text::Template::Simple::Compiler::Safe;
 # Safe compiler. Totally experimental
+use strict;
 
 sub _compile { shift; return __PACKAGE__->_object->reval(shift) }
 
@@ -51,6 +61,141 @@ sub _permit {
       return $class->permit;
    }
    return @permit;
+}
+
+package Text::Template::Simple::Caller;
+use strict;
+
+use constant PACKAGE    => 0;
+use constant FILENAME   => 1;
+use constant LINE       => 2;
+use constant SUBROUTINE => 3;
+use constant HASARGS    => 4;
+use constant WANTARRAY  => 5;
+use constant EVALTEXT   => 6;
+use constant IS_REQUIRE => 7;
+use constant HINTS      => 8;
+use constant BITMASK    => 9;
+
+use Carp qw( croak );
+
+sub stack {
+   my $self    = shift;
+   my $opt     = shift || {};
+   die "Parameters to stack() must be a HASH" if ref($opt) ne 'HASH';
+   my $frame   = $opt->{frame};
+   my $type    = $opt->{type} || '';
+      $frame ||= 0;
+   my(@callers, $context);
+
+   while ( my @c = caller ++$frame ) {
+
+      foreach my $id ( 0 .. $#c ) {
+         next if $id == WANTARRAY;
+         $c[$id] ||= '';
+      }
+
+      $context = defined $c[WANTARRAY] ?  ( $c[WANTARRAY] ? 'LIST' : 'SCALAR' )
+               :                            'VOID'
+               ;
+
+      push  @callers,
+            {
+               class    => $c[PACKAGE],
+               file     => $c[FILENAME],
+               line     => $c[LINE],
+               sub      => $c[SUBROUTINE],
+               context  => $context,
+               isreq    => $c[IS_REQUIRE],
+               hasargs  => $c[HASARGS] ? 'YES' : 'NO',
+               evaltext => $c[EVALTEXT],
+               hints    => $c[HINTS],
+               bitmask  => $c[BITMASK],
+            };
+
+   }
+
+   return reverse @callers if ! $type;
+
+   if ( $self->can( my $method = '_' . $type ) ) {
+      return $self->$method( $opt, \@callers );
+   }
+
+   die "Unknown caller stack type: $type";
+}
+
+sub _string {
+   my $self    = shift;
+   my $opt     = shift;
+   my $callers = shift;
+   my $is_html = shift;
+
+   my $name = $opt->{name} ? "FOR $opt->{name} " : "";
+   my $rv   = qq{[ DUMPING CALLER STACK $name]\n\n};
+
+   foreach my $c ( reverse @{$callers} ) {
+      $rv .= sprintf qq{%s %s() at %s line %s\n},
+                     $c->{context},
+                     $c->{sub},
+                     $c->{file},
+                     $c->{line};
+   }
+
+   $rv = "<!-- $rv -->" if $is_html;
+   return $rv;
+}
+
+sub _html_comment {
+   shift->_string( @_, 'add html comment' );
+}
+
+sub _html_table {
+   warn "Caller stack type 'html_table' is not yet implemented. "
+       ."Changing the option to 'string' instead";
+   shift->_string( @_ );
+}
+
+sub _text_table {
+   my $self    = shift;
+   my $opt     = shift;
+   my $callers = shift;
+   eval { require Text::Table; };
+   croak "Caller stack type 'text_table' requires Text::Template" if $@;
+
+   my $table = Text::Table->new( qw(
+                  | CONTEXT    | SUB      | LINE  | FILE    | HASARGS
+                  | IS_REQUIRE | EVALTEXT | HINTS | BITMASK |
+               ));
+
+   foreach my $c ( reverse @{$callers} ) {
+      $table->load(
+         [
+           '|', $c->{context},
+           '|', $c->{sub},
+           '|', $c->{line},
+           '|', $c->{file},
+           '|', $c->{hasargs},
+           '|', $c->{isreq},
+           '|', $c->{evaltext},
+           '|', $c->{hints},
+           '|', $c->{bitmask},
+           '|'
+         ],
+      );
+   }
+
+   my $name = $opt->{name} ? "FOR $opt->{name} " : "";
+   my $top  = qq{| DUMPING CALLER STACK $name |\n};
+
+   my $rv   = "\n" . ( '-' x (length($top) - 1) ) . "\n" . $top
+            . $table->rule( '-', '+')
+            . $table->title
+            . $table->rule( '-', '+')
+            . $table->body
+            . $table->rule( '-', '+')
+            ;
+
+   return $rv;
 }
 
 package Text::Template::Simple;
@@ -85,15 +230,21 @@ use constant CID            => ++$OID;
 use constant FILENAME       => ++$OID;
 use constant RESUME         => ++$OID;
 use constant IOLAYER        => ++$OID;
+use constant STACK          => ++$OID;
 # number of the last object field
 use constant MAXOBJFIELD    =>   $OID;
 
 use Carp     qw(croak);
 use Exporter qw();
 
-require Win32 if IS_WINDOWS;
+BEGIN {
+   if ( IS_WINDOWS ) {
+      local $@;
+      eval { require Win32; Win32->import };
+   }
+}
 
-$VERSION     = '0.48';
+$VERSION     = '0.49_01';
 @ISA         = qw( Exporter );
 
 %EXPORT_TAGS = (
@@ -159,6 +310,7 @@ my %DEFAULT = (
       fix_uncuddled =>  0, # do some worst practice?
       resume        =>  0, # resume on error?
       iolayer       => '', # I/O layer for filehandles
+      stack         => '',
 );
 
 my %ERROR = (
@@ -849,6 +1001,7 @@ sub _parse {
    my $self      = shift;
    my $tmp       = shift;
    my $map_keys  = shift; # code sections are hash keys
+   my $cache_id  = shift;
    my $finit     = '';    # map_keys init code
       $finit     = q~ || ''~ if $map_keys && $map_keys eq 'init';
    my $is_code   = 0; # we are inside a code section
@@ -974,7 +1127,9 @@ sub _parse {
    $code_start  = "package $ATTR{N_DUMMY};";
    $code_start .= 'use strict;' if $self->[STRICT];
    $code_start .= 'sub { ';
-   $code_start .= $self->[HEADER].';' if $self->[HEADER];
+
+   $code_start .= $self->_add_stack( $cache_id )  if $self->[STACK];
+   $code_start .= $self->[HEADER].';'             if $self->[HEADER];
    $code_start .= "my $faker;";
    $code_start .= "my $ATTR{FAKER_HASH} = {\@_};" if $map_keys;
    $code_start .= "\n#line 1 " .  $self->[FILENAME] . "\n";
@@ -982,6 +1137,23 @@ sub _parse {
    warn "\n\n#FRAGMENT\n$fragment\n#/FRAGMENT\n"  if IS_DEBUG > 1;
 
    return $fragment;
+}
+
+sub _add_stack {
+   my $self    = shift;
+   my $cs_name = shift || '<ANON TEMPLATE>';
+   my $stack   = $self->[STACK] || '';
+   return if lc($stack) eq 'off';
+   my $type    = ($stack eq '1' || $stack eq 'yes' || $stack eq 'on')
+               ? 'string'
+               : $stack
+               ;
+
+   foreach my $e ( $cs_name, $type ) {
+      $e =~ s{'}{\\'}xmsg;
+   }
+
+   return "warn stack( { type => '$type', name => '$cs_name' } );";
 }
 
 sub _set_resume_re {
@@ -1373,6 +1545,24 @@ reformat the data with cuddled versions before parsing it.
 This option does not have any effect under perls older than C<5.8.0>.
 Set this to C<utf8> (no initial colon) if your I/O is C<UTF-8>. 
 Not tested with other encodings.
+
+=head3 stack
+
+This option enables caller stack tracing for templates. The generated
+list is sent to C<warn>. So, it is possible to capture
+this data with a signal handler. Available options are:
+
+   string
+   html_comment
+   html_table
+   text_table  
+
+C<html_comment> is the same as C<string> except that it also includes HTML
+comment markers. C<text_table> needs the optional module C<Text::Table>.
+
+This option is also available to all templates as a function named
+C<stack> for individual stack dumping.
+But, currently this interface is not documented.
 
 =head2 compile DATA [, FILL_IN_PARAM, OPTIONS]
 
---
+++ t/07-stack.t
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl -w
+# Simple test. Just try to use the module.
+use strict;
+use Test::More qw( no_plan );
+use Text::Template::Simple; 
+
+ok(simple('string'), "String Dumper");
+ok(simple('html_comment'),"HTML Comment Dumper");
+ok(simple('html_table'), "HTML Table Dumper");
+
+eval { require Text::Table; };
+
+if ( ! $@ ) {
+   ok(simple('text_table'), "Text Table Dumper");
+}
+
+sub simple {
+   my $type = shift || die "type?";
+   my $template = Text::Template::Simple->new(
+      header   => q~my $foo = shift; my $bar = shift;~,
+      add_args => ['bar',['baz']],
+      stack    => $type,
+   );
+   my $result = $template->compile('t/test.tmpl', ['Burak']);
+   #warn "[COMPILED] $result\n";
+   return $result;
+}
+
+exit;
+
+__END__
